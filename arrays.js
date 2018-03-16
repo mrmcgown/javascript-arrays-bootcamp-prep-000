1c3d7cc39c5010825d2818ff29b36361739f0bea
Michael R. McGown <mrmcgown@gmail.com>
2018-03-16 16:15:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -26,3 +26,6 @@ function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) {
   return chocolateBars.shift();
 }
 
+function removeElementFromBeginningOfArray(chocolateBars) {
+  return chocolateBars.slice(1)
+}
