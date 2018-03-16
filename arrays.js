ce91398578937cc5fda30cc0e5866d5461d37e08
Michael R. McGown <mrmcgown@gmail.com>
2018-03-16 15:47:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+let chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
   ["element", ...array];
