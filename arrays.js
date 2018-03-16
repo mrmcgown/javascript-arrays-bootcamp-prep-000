c9004dbe636fb6e49ccb9375c3120685b25f1f5a
Michael R. McGown <mrmcgown@gmail.com>
2018-03-16 15:47:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
   ["element", ...array];
-};
+}
 
 function destructivelyAddElementToBeginningOfArray(chocolateBars, "milky way") {
   chocolateBars.unshift("milky way");
