bf570640f1fcfb53d6b90df5344bb5faddd5d1c0
Michael R. McGown <mrmcgown@gmail.com>
2018-03-16 16:23:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,39 +1,42 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-
-function addElementToBeginningOfArray(chocoloateBars, foo) {
-  return ["foo", ...chocolateBars];
+//var candyString = "foo"
+ 
+function addElementToBeginningOfArray(chocolateBars, candyString){
+  return [candyString, ...chocolateBars];
 }
-
-function destructivelyAddElementToBeginningOfArray(chocolateBars, foo) {
-  chocolateBars.unshift("foo");
+ 
+function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
+  chocolateBars.unshift(candyString);
   return chocolateBars;
 }
-
-function addElementToEndOfArray(chocolateBars, foo) {
-  return [...chocolateBars, "foo"];
+ 
+function addElementToEndOfArray(chocolateBars, candyString){
+  return [...chocolateBars, candyString];
 }
-
-function destructivelyAddElementToEndOfArray(chocolateBars, foo) {
-  chocolateBars.push("foo");
+ 
+function destructivelyAddElementToEndOfArray(chocolateBars, candyString){
+  chocolateBars.push(candyString);
   return chocolateBars;
 }
-
-function accessElementInArray(array, index) {
+ 
+function accessElementInArray(array, index){
   return array[index];
 }
-
-function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) {
-  return chocolateBars.shift();
+ 
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
+  chocolateBars.shift();
+  return chocolateBars;
 }
-
-function removeElementFromBeginningOfArray(chocolateBars) {
-  return chocolateBars.slice(1)
+ 
+function removeElementFromBeginningOfArray(chocolateBars){
+  return chocolateBars.slice(1);
 }
-
-function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
-  return chocolateBars.pop();
+ 
+function destructivelyRemoveElementFromEndOfArray(chocolateBars){
+  chocolateBars.pop();
+  return chocolateBars;
 }
-
-function removeElementFromEndOfArray(chocolateBars) {
-  chocolateBars.slice(0, chocolateBars.length - 1);
+ 
+function removeElementFromEndOfArray(chocolateBars){
+  return chocolateBars.slice(0, chocolateBars.length - 1);
 }
