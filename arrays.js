0074b775dae8c918a6b8fdc2d7534d6b89987754
Michael R. McGown <mrmcgown@gmail.com>
2018-03-16 15:59:51 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(chocoloateBars, milkyway) {
+function addElementToBeginningOfArray(chocoloateBars, foo) {
   return [milkyway, ...chocolateBars];
 }
 
